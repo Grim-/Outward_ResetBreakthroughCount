2f0f1be1080265150f2ba2cdaa297d3bb3ab8ee1
Bryan <pheonix2105+github@gmail.com>
2019-04-07 20:38:18 +0100

    First Push

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using Partiality.Modloader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DisableBreakThroughCount
+{
+    public class DisableBreakThroughCount : PartialityMod
+    {
+        public static ScriptLoad qsmPatch;
+
+        public DisableBreakThroughCount()
+        {
+            this.ModID = "Reset Breakthrough Points";
+            this.Version = "0.1";
+            this.author = "Emo #Discord : Emo #7953";
+        }
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+
+            ScriptLoad.disableBT = this;
+
+
+            GameObject obj = new GameObject();
+            qsmPatch = obj.AddComponent<ScriptLoad>();
+            qsmPatch.Initialise();
+
+            Debug.Log(":: Reset Breakthrough Points ENABLED ::");
+        }
+
+    }
+}
+
---
+++ ScriptLoad.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using On;
+using UnityEngine;
+
+namespace DisableBreakThroughCount
+{
+    public class ScriptLoad : MonoBehaviour
+    {
+        public static DisableBreakThroughCount disableBT;
+
+        public void Initialise()
+        {
+            Debug.Log(":: Reset Breakthrough Points INIT ::");
+
+            Patch();
+        }
+
+        public void Patch()
+        {
+            //Put in patched methods here
+            On.PlayerCharacterStats.OnUpdateStats += new On.PlayerCharacterStats.hook_OnUpdateStats(onUpdateStatsHook);
+        }
+
+        private void onUpdateStatsHook(On.PlayerCharacterStats.orig_OnUpdateStats orig, PlayerCharacterStats self)
+        {
+            orig(self);
+
+            if (Input.GetKeyDown(KeyCode.F12))
+            {
+                FieldInfo pcStatsInfo = typeof(PlayerCharacterStats).GetField("m_usedBreakthroughCount", BindingFlags.Instance | BindingFlags.NonPublic);
+
+                Debug.Log("Breakthrough use count : BEFORE " + pcStatsInfo.GetValue(self));
+                pcStatsInfo.SetValue(self, 0);
+                Debug.Log("Breakthrough use count : AFTER " + pcStatsInfo.GetValue(self));
+            }
+        }
+    }
+}
